b5f31b644308f7ca8e4d57b601e395e9a99dff50
Tim Burke <timburke@alumni.rice.edu>
2017-09-19 18:36:59 -0700

    Add tests on python 3.4 and 3.5

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
